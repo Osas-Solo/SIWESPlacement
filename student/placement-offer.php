a5614fecb8c1091d829e238681ea7a09cfafc2a0
Osaremhen Ukpebor <osas.solo@outlook.com>
2022-07-19 12:55:03 +0100

    Added placement-offer.php

--- student/placement-offer.php
+++ student/placement-offer.php
@@ -56,29 +56,9 @@ $placement_offer = new PlacementOffer($database_connection, $placement_offer_id)
                         </div>
         
                         <div class="">
-                            <h4 class="mb-4">Apply For The Job</h4>
-                            <form>
-                                <div class="row g-3">
-                                    <div class="col-12 col-sm-6">
-                                        <input type="text" class="form-control" placeholder="Your Name">
-                                    </div>
-                                    <div class="col-12 col-sm-6">
-                                        <input type="email" class="form-control" placeholder="Your Email">
-                                    </div>
-                                    <div class="col-12 col-sm-6">
-                                        <input type="text" class="form-control" placeholder="Portfolio Website">
-                                    </div>
-                                    <div class="col-12 col-sm-6">
-                                        <input type="file" class="form-control bg-white">
-                                    </div>
-                                    <div class="col-12">
-                                        <textarea class="form-control" rows="5" placeholder="Coverletter"></textarea>
-                                    </div>
-                                    <div class="col-12">
-                                        <button class="btn btn-primary w-100" type="submit">Apply Now</button>
-                                    </div>
-                                </div>
-                            </form>
+                            <div class="col-12">
+                                <button class="btn btn-primary w-100" type="button">Request Placement</button>
+                            </div>
                         </div>
                     </div>
         
@@ -96,10 +76,6 @@ $placement_offer = new PlacementOffer($database_connection, $placement_offer_id)
                             </p>
                             <p><i class="fa fa-angle-right text-primary me-2"></i><b>Location:</b>
                                 <?php echo $placement_offer->organisation->address?></p>
-                        </div>
-                        <div class="bg-light rounded p-5 wow slideInUp" data-wow-delay="0.1s">
-                            <h4 class="mb-4">Company Detail</h4>
-                            <p class="m-0">Ipsum dolor ipsum accusam stet et et diam dolores, sed rebum sadipscing elitr vero dolores. Lorem dolore elitr justo et no gubergren sadipscing, ipsum et takimata aliquyam et rebum est ipsum lorem diam. Et lorem magna eirmod est et et sanctus et, kasd clita labore.</p>
                         </div>
                     </div>
                 </div>
